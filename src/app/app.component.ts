a61c6ffa08e1f5a8202ae9352e27768722d95eaa
AfonSir <afonsir17@gmail.com>
2021-04-23 22:16:02 -0300

    refactor: move task model to specific file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gerenciador de Tarefas';
-  task: Task = new Task(12, 'foo');
-}
-
-export class Task {
-  public id: number;
-  public title: string;
-
-  constructor(id: number, title: string) {
-    this.id = id;
-    this.title = title;
-  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 
-const tasks: any[] = [
+import { Task } from "./shared/task.model";
+
+const tasks: Task[] = [
   { id: 1, title: "Task 1" },
   { id: 2, title: "Task 2" },
   { id: 3, title: "Task 3" },
